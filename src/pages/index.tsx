8d6ea9e2c221599b61449501a1d88a1a1725f00e
Damon Blais <damon@rethunk.tech>
2023-12-09 17:28:18 -0800

    refactor(components): Update homepage layout and remove Footer component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,87 +1,108 @@
-import HomeRounded from '@mui/icons-material/HomeRounded'
+
 import Box from '@mui/material/Box'
-import Typography from '@mui/material/Typography'
-import Footer from 'components/example/Footer'
+import type { SxProps } from '@mui/system'
+import CornerAccents from 'components/branding/CornerAccents'
+import TypedText from 'components/branding/TypedText'
 import Link from 'components/mui/Link'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 
-const styles = {
+const styles: { [key: string]: SxProps } = {
   container: {
-    display:    'grid',
-    height:     '100vh',
-    placeItems: 'center',
-    textAlign:  'center',
+    alignItems:     'center',
+    display:        'flex',
+    flexDirection:  'column',
+    gap:            10,
+    justifyContent: 'center',
+    minHeight:      '100vh',
+    textAlign:      'center',
+    userSelect:     'none',
   },
-
-  typography: {
-    my: 4,
-
-    // no marinBottom on last of type
-    '&:last-of-type': {
-      mb: 0,
+  titleBox: {
+    display:  'inline-block',
+    position: 'relative',
+    width:    'auto',
+  },
+  titleText: {
+    border:     '12px solid #F00',
+    color:      '#F00',
+    fontSize:   'clamp(1.5rem, 8vw, 7.5rem)',
+    fontWeight: 'bold',
+    lineHeight: 0.8,
+    padding:    '0.5rem 0.5rem',
+    textShadow: '0 0 2px black',
+    transform:  'perspective(300px) rotateX(10deg)',
+    'span':     {
+      letterSpacing: '0.75rem',
+      paddingLeft:   '0.4rem',
     },
-
-    // link
-    '& a': {
-      textDecoration: 'none',
+  },
+  linksBox: {
+    display:       'flex',
+    flexDirection: 'column',
+    fontFamily:    'VT323, monospace',
+    fontSize:      'clamp(1rem, 5vw, 2rem)',
+    gap:           4,
+    lineHeight:    1,
+    marginRight:   '40vw',
+    minHeight:     'clamp(10rem, 25vh, 20rem)',
+    minWidth:      'clamp(20rem, 40vw, 60rem)',
+    textAlign:     'left',
+    letterSpacing: '0.1rem',
+    wordSpacing:   '0.2rem',
+    '& > *':       {
+      padding: '0.25rem 0',
     },
-
-    // code
-    '& code': {
-      border:       '1px solid',
-      borderColor:  'secondary.light',
-      borderRadius: 1,
-      color:        'secondary.main',
-      display:      'inline-block',
-      px:           1,
-      py:           0.5,
+    '& > *:hover': {
+      background: 'var(--accent)',
+      color:      '#440000',
+      cursor:     'pointer',
     },
   },
 }
 
+const linkItems = [
+  {
+    href:  'https://store.steampowered.com/app/1966720/Lethal_Company/',
+    label: '> Lethal Company (video game) [Steam]',
+  },
+  {
+    href:  'https://github.com/LethalCompany/LethalCompanyTemplate',
+    label: '> Mod Repository Template [GitHub]',
+  },
+  {
+    href:  '',
+    label: '> Unofficial Community [Discord]',
+  },
+]
 
 const Home: NextPage = (): JSX.Element => {
   return <>
     <Head>
-      <title>Boilerplate by Rethunk.Tech</title>
+      <title>Your Source for Lethal Company Modding Resources</title>
     </Head>
 
-    {/* full-height container with centered content */}
+    <CornerAccents />
+
     <Box sx={styles.container}>
-      {/* inner container holds centered content together */}
-      <Box>
-        <HomeRounded sx={{ fontSize: '14rem' }} />
+      <Box sx={styles.titleBox}>
+        <Box sx={styles.titleText}>
+          <span>LETHAL</span><br />MODDING
+        </Box>
+      </Box>
 
-        <Typography component="h1" sx={styles.typography} variant="h2">
-          <Link
-            href="https://nextjs.org"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            Next.js
-          </Link> + <Link
-            href="https://mui.com"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            MUI
-          </Link> + <Link
-            href="https://typescriptlang.org"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            TypeScript
-          </Link>!
-        </Typography>
-
-        <Typography component="h4" sx={styles.typography} variant="h4">
-          Get started by editing <code>pages/index.tsx</code>
-        </Typography>
+      <Box sx={styles.linksBox}>
+        {linkItems.map((item, index) => (
+          <Link color="inherit" href={item.href} key={index} underline="none">
+            <TypedText
+              finalText={item.label}
+              startDelay={index * 1000}
+              typingSpeed={25}
+            />
+          </Link>
+        ))}
       </Box>
     </Box>
-
-    <Footer />
   </>
 }
 
