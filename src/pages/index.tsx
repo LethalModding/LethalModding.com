d669251d59b7a3e3b63137f4929a02bc3bf55402
Damon Blais <damon@rethunk.tech>
2023-12-13 21:07:09 -0800

    refactor(homepage): Streamline styling and improve link categorization

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,82 +1,68 @@
-
 import Box from '@mui/material/Box'
-import type { SxProps } from '@mui/system'
+import Typography from '@mui/material/Typography'
 import CornerAccents from 'components/branding/CornerAccents'
 import TypedText from 'components/branding/TypedText'
 import Link from 'components/mui/Link'
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import useGlobalStyles from 'styles/globalStyles'
 
-const styles: { [key: string]: SxProps } = {
-  container: {
-    alignItems:     'center',
-    display:        'flex',
-    flexDirection:  'column',
-    gap:            10,
-    justifyContent: 'center',
-    minHeight:      '100vh',
-    textAlign:      'center',
-    userSelect:     'none',
-  },
+const styles = {
   titleBox: {
-    display:  'inline-block',
-    position: 'relative',
-    width:    'auto',
+    color:      '#E00',
+    display:    'inline-block',
+    fontSize:   'clamp(1rem, 7vw, 4rem)',
+    lineHeight: '0.8',
+    mx:         'auto',
+    textShadow: '4px 0 4px white',
   },
+
   titleText: {
-    border:     '12px solid #F00',
-    color:      '#F00',
-    fontSize:   'clamp(1.5rem, 8vw, 7.5rem)',
-    fontWeight: 'bold',
-    lineHeight: 0.8,
-    padding:    '0.5rem 0.5rem',
-    textShadow: '0 0 2px black',
-    transform:  'perspective(300px) rotateX(10deg)',
+    border:     '10px solid #E00',
+    fontFamily: 'sans-serif',
+    padding:    '0.5rem 1rem',
+    paddingTop: '1rem',
+    transform:  'perspective(300px) rotateX(15deg)',
     'span':     {
-      letterSpacing: '0.75rem',
-      paddingLeft:   '0.4rem',
-    },
-  },
-  linksBox: {
-    display:       'flex',
-    flexDirection: 'column',
-    fontFamily:    'VT323, monospace',
-    fontSize:      'clamp(1rem, 5vw, 2rem)',
-    gap:           4,
-    lineHeight:    1,
-    marginRight:   '40vw',
-    minHeight:     'clamp(10rem, 25vh, 20rem)',
-    minWidth:      'clamp(20rem, 40vw, 60rem)',
-    textAlign:     'left',
-    letterSpacing: '0.1rem',
-    wordSpacing:   '0.2rem',
-    '& > *':       {
-      padding: '0.25rem 0',
-    },
-    '& > *:hover': {
-      background: 'var(--accent)',
-      color:      '#440000',
-      cursor:     'pointer',
+      letterSpacing: '0.08em',
+      paddingLeft:   '0.3em',
     },
   },
 }
 
-const linkItems = [
+const externalLinks = [
   {
     href:  'https://store.steampowered.com/app/1966720/Lethal_Company/',
-    label: '> Lethal Company (video game) [Steam]',
+    label: '> Buy Lethal Company on Steam',
   },
   {
     href:  'https://github.com/LethalCompany/LethalCompanyTemplate',
-    label: '> Mod Repository Template [GitHub]',
+    label: '> Clone the Template Repository',
   },
   {
-    href:  '',
-    label: '> Unofficial Community [Discord]',
+    href:  'https://discord.gg/nYcQFEpXfU',
+    label: '> Join the Unofficial Discord',
+  },
+]
+
+const internalLinks = [
+  {
+    href:  '/mods',
+    label: '> Find Mods',
+  },
+  {
+    href:  '/guides',
+    label: '> Read Guides',
+  },
+  {
+    href:  '/tools',
+    label: '> Use Tools',
   },
 ]
 
 const Home: NextPage = (): JSX.Element => {
+  const globalStyles = useGlobalStyles()
+
   return <>
     <Head>
       <title>Your Source for Lethal Company Modding Resources</title>
@@ -84,23 +70,65 @@ const Home: NextPage = (): JSX.Element => {
 
     <CornerAccents />
 
-    <Box sx={styles.container}>
-      <Box sx={styles.titleBox}>
-        <Box sx={styles.titleText}>
-          <span>LETHAL</span><br />MODDING
+    <Box sx={{
+      ...globalStyles.container,
+      display:    'grid',
+      placeItems: 'center',
+    }}
+    >
+      <Box sx={{ display: 'grid' }}>
+        <Box sx={styles.titleBox}>
+          <Box component="h1" sx={styles.titleText}>
+            <span>LETHAL</span><br />MODDING
+          </Box>
         </Box>
-      </Box>
 
-      <Box sx={styles.linksBox}>
-        {linkItems.map((item, index) => (
-          <Link color="inherit" href={item.href} key={index} underline="none">
-            <TypedText
-              finalText={item.label}
-              startDelay={index * 1000}
-              typingSpeed={25}
-            />
-          </Link>
-        ))}
+        <Box sx={globalStyles.linksBox}>
+          <Box className="column">
+            <Typography variant="h3">
+              <TypedText
+                finalText="Internal"
+                startDelay={0}
+                typingSpeed={17}
+              />
+            </Typography>
+            {internalLinks.map((item, index) => <Link
+              color="inherit"
+              href={item.href}
+              key={index}
+              underline="none"
+            >
+              <TypedText
+                finalText={item.label}
+                startDelay={index * 800 + 400}
+                typingSpeed={17}
+              />
+            </Link>)}
+          </Box>
+
+          <Box className="column">
+            <Typography variant="h3">
+              <TypedText
+                finalText="External"
+                startDelay={200}
+                typingSpeed={17}
+              />
+            </Typography>
+            {externalLinks.map((item, index) => <Link
+              color="inherit"
+              href={item.href}
+              key={index}
+              target="_blank"
+              underline="none"
+            >
+              <TypedText
+                finalText={item.label}
+                startDelay={index * 800 + 600}
+                typingSpeed={17}
+              />
+            </Link>)}
+          </Box>
+        </Box>
       </Box>
     </Box>
   </>
