13b5836415d9e9c5e5761eb1ec30b279dad7cec0
Damon Blais <damon@rethunk.tech>
2023-12-13 21:06:19 -0800

    style: Update light theme with VT323 font and adjust font size

--- src/styles/lightThemeOptions.ts
+++ src/styles/lightThemeOptions.ts
@@ -5,6 +5,10 @@ const lightThemeOptions: Theme = createTheme({
   palette: {
     mode: 'light',
   },
+  typography: {
+    fontFamily: ['VT323', 'monospace'].join(','),
+    fontSize:   24,
+  },
 })
 
 export default responsiveFontSizes(lightThemeOptions)
