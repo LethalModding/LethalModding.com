7e6652fee1752ad371c6c584f4a96de7be4d71c5
Damon Blais <damon@rethunk.tech>
2024-02-19 21:25:19 -0800

    feat(api): Add endpoint for recommended mods

---
+++ src/pages/api/concrete/mods/recommended.ts
@@ -0,0 +1,41 @@
+import { type NextApiRequest, type NextApiResponse } from 'next/types'
+
+const mods = [
+  'BepInEx/BepInExPack',
+  'Evaisa/HookGenPatcher',
+  'CharlesE2/HostFixes',
+
+  'kuba6000/LC_Masked_Fix',
+  'ThePotato/scanForItemsFix',
+  'Hamunii/JetpackFallFix',
+  'ViViKo/ItemClippingFix',
+  'Dev1A3/RecentlyPlayedWith',
+  'ShaosilGaming/FlashlightFix',
+  'EliteMasterEric/SlimeTamingFix',
+  'FutureSavior/Boombox_Sync_Fix',
+  'linkoid/DissonanceLagFix',
+  'Zaggy1024/PathfindingLagFix',
+
+  'itsmeowdev/DoorFix',
+  'monkes_mods/JumpDelayPatch',
+
+  'flerouwu/LC_FastStartup',
+  'BlueAmulet/LogNeuter',
+  'Dev1A3/LobbyInviteOnly',
+  'taffyko/QuickQuitToMenu',
+]
+
+export default async function ConcreteModsRecommended(
+  req: NextApiRequest,
+  res: NextApiResponse
+): Promise<void> {
+  if (req.method === 'OPTIONS') {
+    return res.status(204).json({ status: 'ok' })
+  }
+
+  if (req.method !== 'GET') {
+    return res.status(405).json({ error: 'Method Not Allowed' })
+  }
+
+  res.status(200).json(mods)
+}
