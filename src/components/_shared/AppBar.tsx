ebe3af47732e53867b562c45f2c43230febbe1f9
Damon Blais <damon@rethunk.tech>
2024-03-11 04:01:12 -0700

    feat(AppBar): Add responsive menu for mobile navigation

--- src/components/_shared/AppBar.tsx
+++ src/components/_shared/AppBar.tsx
@@ -1,13 +1,20 @@
+import MenuIcon from '@mui/icons-material/Menu'
+import MenuOpenIcon from '@mui/icons-material/MenuOpen'
 import MuiAppBar from '@mui/material/AppBar'
 import ListItemButton from '@mui/material/ListItemButton'
+import Menu from '@mui/material/Menu'
+import MenuItem from '@mui/material/MenuItem'
 import { Theme } from '@mui/material/styles'
 import useMediaQuery from '@mui/system/useMediaQuery'
 import Link from 'components/mui/Link'
 import Image from 'next/image'
+import { useState } from 'react'
 import AccountButton from './auth/AccountButton'
 
 export default function AppBar(): JSX.Element {
   const isMobile = useMediaQuery((theme: Theme) => theme.breakpoints.down('sm'))
+
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
 
   return <MuiAppBar
     position="sticky"
@@ -47,23 +54,68 @@ export default function AppBar(): JSX.Element {
       </Link>
     </ListItemButton>
 
-    <ListItemButton>
-      <Link href="https://discord.gg/lcmod">
-        Join the Discord
-      </Link>
-    </ListItemButton>
+    {isMobile ? <>
+      <ListItemButton onClick={(e) => setAnchorEl(e.currentTarget)}>
+        {anchorEl ? <MenuOpenIcon /> : <MenuIcon />}
+      </ListItemButton>
 
-    <ListItemButton>
-      <Link href="/tools">
-        Search Thunderstore
-      </Link>
-    </ListItemButton>
+      <Menu
+        anchorEl={anchorEl}
+        open={!!anchorEl}
+        onClose={() => setAnchorEl(null)}
+        sx={{
+          'a': {
+            color:          'inherit',
+            textDecoration: 'none',
 
-    <ListItemButton>
-      <Link href="/team">
-        Your Team
-      </Link>
-    </ListItemButton>
+            '&:hover': {
+              backgroundColor: 'unset',
+            }
+          },
+
+          '.MuiMenuItem-root': {
+            px: 3,
+            py: 0,
+          }
+        }}
+      >
+        <MenuItem>
+          <Link href="https://discord.gg/lcmod">
+            Join the Discord
+          </Link>
+        </MenuItem>
+
+        <MenuItem>
+          <Link href="/tools">
+            Search Thunderstore
+          </Link>
+        </MenuItem>
+
+        <MenuItem>
+          <Link href="/team">
+            Your Team
+          </Link>
+        </MenuItem>
+      </Menu>
+    </> : <>
+      <ListItemButton>
+        <Link href="https://discord.gg/lcmod">
+          Join the Discord
+        </Link>
+      </ListItemButton>
+
+      <ListItemButton>
+        <Link href="/tools">
+          Search Thunderstore
+        </Link>
+      </ListItemButton>
+
+      <ListItemButton>
+        <Link href="/team">
+          Your Team
+        </Link>
+      </ListItemButton>
+    </>}
 
     <AccountButton />
   </MuiAppBar>
