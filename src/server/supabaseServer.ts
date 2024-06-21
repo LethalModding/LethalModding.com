130f3c02aa899967ff1f52da26625c338ffe5c1b
Damon Blais <damon@rethunk.tech>
2024-06-20 21:44:56 -0700

    style(server): change double quotes to single quotes in supabaseServer.ts

--- src/server/supabaseServer.ts
+++ src/server/supabaseServer.ts
@@ -1,4 +1,4 @@
-import { createClient } from "@supabase/supabase-js"
+import { createClient } from '@supabase/supabase-js'
 
 export const supabaseSERVER = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL ?? '',
