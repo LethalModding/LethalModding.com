af5c276b792d193fb23a3b1a0d7f6a9771f3ca3f
Damon Blais <damon@rethunk.tech>
2024-06-20 20:27:02 -0700

    feat(team): Add more pages, update structure

--- src/pages/team/index.tsx
+++ src/pages/team/index.tsx
@@ -8,7 +8,8 @@ import TeamProfilePage from 'components/Team/Profile'
 import Link from 'components/mui/Link'
 import ProjectManagePage from 'components/project/Manage'
 import Head from 'next/head'
-import { useMemo, useState } from 'react'
+import { useRouter } from 'next/router'
+import { useEffect, useMemo, useState } from 'react'
 import { useAppStore } from 'store'
 
 const TeamPage = (): JSX.Element => {
@@ -28,58 +29,87 @@ const TeamPage = (): JSX.Element => {
         return null
     }
   }, [selectedPage])
+  const router = useRouter()
+  useEffect(() => {
+    if (router.query.page) {
+      setSelectedPage((router.query.page as string[]).join('/'))
+    }
+  }, [router.query.page])
 
   const selectedTeam = useAppStore(state => state.selectedTeam)
 
-  return <>
-    <Head>
-      <title>Your Source for Lethal Company Mods</title>
-    </Head>
+  return (
+    <>
+      <Head>
+        <title>Your Source for Lethal Company Mods</title>
+      </Head>
 
-    <Box
-      sx={{
-        display:       'flex',
-        flexDirection: 'column',
-        height:        'calc(100vh - 56px)',
-
-        overflowX: 'hidden',
-        overflowY: 'auto',
-
-        '&::-webkit-scrollbar': {
-          width:  '0.25em',
-          height: '0.25em',
-        },
-
-        '&::-webkit-scrollbar-thumb': {
-          backgroundColor: 'var(--accent)',
-        },
-      }}
-    >
-      <Paper
-        elevation={4}
+      <Box
         sx={{
-          p: 4,
+          display: 'flex',
+          flexDirection: 'column',
+          height: 'calc(100vh - 56px)',
+
+          overflowX: 'hidden',
+          overflowY: 'auto',
+
+          '&::-webkit-scrollbar': {
+            width: '0.25em',
+            height: '0.25em',
+          },
+
+          '&::-webkit-scrollbar-thumb': {
+            backgroundColor: 'var(--accent)',
+          },
         }}
       >
-        <Typography variant="h4">
-          {selectedPage !== '' ? <>
-            <Link
-              onClick={() => setSelectedPage('')}
-              sx={{ textDecoration: 'none' }}
-            >
-              {selectedTeam?.name || 'Team'}
-            </Link>
-            {' > '}
-            {selectedPage.charAt(0).toUpperCase() + selectedPage.slice(1)}
-          </> : selectedTeam?.name || 'Team'}
-        </Typography>
-      </Paper>
+        <Paper
+          elevation={4}
+          sx={{
+            p: 4,
+          }}
+        >
+          <Typography variant="h4">
+            {selectedPage !== '' ? (
+              <>
+                <Link
+                  onClick={() => setSelectedPage('')}
+                  sx={{ textDecoration: 'none' }}
+                >
+                  {selectedTeam?.name || 'Team'}
+                </Link>
+                {' > '}
+                {selectedPage.charAt(0).toUpperCase() + selectedPage.slice(1)}
+              </>
+            ) : (
+              selectedTeam?.name || 'Team'
+            )}
+          </Typography>
+        </Paper>
 
-      {pageComponent
-        ? pageComponent
-        : <TeamMenu setSelectedPage={setSelectedPage} />}
-    </Box>
-  </>
+        {selectedPage === 'create' ? (
+          pageComponent
+        ) : selectedTeam === null ? (
+          <Box sx={{ p: 2 }}>
+            <Typography
+              gutterBottom
+              variant="h5"
+            >
+              Please select a team to view or manage.
+            </Typography>
+            <Typography variant="h6">
+              Alternatively, you can{' '}
+              <Link href="/team/create">create a new team</Link>.
+            </Typography>
+          </Box>
+        ) : pageComponent ? (
+          pageComponent
+        ) : (
+          <TeamMenu setSelectedPage={setSelectedPage} />
+        )}
+      </Box>
+    </>
+  )
 }
 
 TeamPage.auth = true
