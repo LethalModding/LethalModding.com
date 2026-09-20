f38fe73b3a7d6bd68840fcba0c1f7199bde92d2c
Damon Blais <damon.blais@gmail.com>
2026-09-19 19:13:06 -0700

    refactor(mui): pass ref as a prop in Link

--- src/components/mui/Link.tsx
+++ src/components/mui/Link.tsx
@@ -1,11 +1,12 @@
 import MuiLink, { type LinkProps } from "@mui/material/Link";
 import NextLink from "next/link";
-import { forwardRef } from "react";
+import type { Ref } from "react";
 
-export const Link = forwardRef<HTMLAnchorElement, LinkProps>((props, ref) => (
+export const Link = ({
+	ref,
+	...props
+}: LinkProps & { ref?: Ref<HTMLAnchorElement> }) => (
 	<NextLink href={props.href ?? "#"} legacyBehavior={true} passHref={true}>
 		<MuiLink ref={ref} {...props} />
 	</NextLink>
-));
-
-Link.displayName = "CustomLink";
+);
