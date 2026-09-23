8fd007fcb018a61bf0c111f4326c82cda27e1097
Damon Blais <damon.blais@gmail.com>
2026-09-22 19:12:21 -0700

    refactor(link): render Next's Link as the MUI Link component instead of legacyBehavior

--- src/components/mui/Link.tsx
+++ src/components/mui/Link.tsx
@@ -2,8 +2,6 @@ import MuiLink, { type LinkProps } from '@mui/material/Link'
 import NextLink from 'next/link'
 import type { Ref } from 'react'
 
-export const Link = ({ ref, ...props }: LinkProps & { ref?: Ref<HTMLAnchorElement> }) => (
-  <NextLink href={props.href ?? '#'} legacyBehavior={true} passHref={true}>
-    <MuiLink ref={ref} {...props} />
-  </NextLink>
+export const Link = ({ ref, href, ...props }: LinkProps & { ref?: Ref<HTMLAnchorElement> }) => (
+  <MuiLink component={NextLink} href={href ?? '#'} ref={ref} {...props} />
 )
